7422f1d5b803240aa80dec92c0139f36fac361ea
Kuzza111 <168814633+Kuzza111@users.noreply.github.com>
2025-07-27 14:09:54 +0300

    Delete plugins/output_terminal.py

--- plugins/output_terminal.py
+++
@@ -1,9 +0,0 @@
-from core.plugin_base import PluginBase
-
-class Plugin(PluginBase):
-    def init(self, core):
-        self.core = core
-        core.event_bus.subscribe('user_input', self.on_action_execute)
-
-    def on_action_execute(self, data):
-        print(f"Output: {data['text']}")
