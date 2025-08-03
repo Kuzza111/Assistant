8d368740ff042a2aea5478a8123fa71c372b1f91
Kuzza111 <168814633+Kuzza111@users.noreply.github.com>
2025-08-04 00:46:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from core.engine import Engine
 
 if __name__ == '__main__':
-    config_path = os.path.join(os.path.dirname(__file__), 'config.yaml')
+    config_path = os.path.join(os.path.dirname(__file__), 'config.json')
     engine = Engine(config_path=config_path)
     engine.load_plugins()
     engine.run()
